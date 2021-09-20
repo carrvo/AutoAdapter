e33fb663011d7cbb6ded3deef8a31170418c314e
carrvo <carrvo@gmail.com>
2021-09-20 17:38:00 -0600

    documentation
    Signed-off-by: carrvo <carrvo@gmail.com>

--- AutoAdapter.DuckTyping/Adapt.cs
+++ AutoAdapter.DuckTyping/Adapt.cs
@@ -9,14 +9,32 @@ using System.Threading.Tasks;
 
 namespace AutoAdapter
 {
+    /// <summary>
+    /// Provides a mechanism for automatically applying
+    /// the ADAPTER PATTERN without custom code.
+    /// </summary>
+    /// <typeparam name="TInterface">
+    /// The interface that will be implemented (ducktyped) by a given object.
+    /// </typeparam>
     public class Adapt<TInterface> : TypeConverter
         where TInterface : class
     {
+        /// <summary>
+        /// Catch the <see cref="ArgumentException"/>
+        /// from <see cref="ConvertFrom(ITypeDescriptorContext, CultureInfo, Object)"/>
+        /// instead of using this method to determine whether it can convert.
+        /// </summary>
+        /// <returns>
+        /// <see cref="true"/>, always.
+        /// </returns>
         public override Boolean CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
         {
             return true;
         }
 
+        /// <summary>
+        /// Applying the ADAPTER PATTERN against <typeparamref name="TInterface"/>.
+        /// </summary>
         public override Object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
             var dyn = new DynamicObject(value);
@@ -30,6 +48,9 @@ namespace AutoAdapter
             }
         }
 
+        /// <summary>
+        /// Applying the ADAPTER PATTERN against <typeparamref name="TInterface"/>.
+        /// </summary>
         public static TInterface From(object value)
         {
             var converter = TypeDescriptor.GetConverter(typeof(TInterface));
@@ -37,20 +58,47 @@ namespace AutoAdapter
         }
     }
 
+    /// <summary>
+    /// Provides a mechanism for automatically applying
+    /// the ADAPTER PATTERN without custom code.
+    /// </summary>
     public class Adapt
     {
         private readonly Object _value;
 
+        /// <summary>
+        /// Creates a FLUENT object that holds the mechanism for automatically
+        /// applying the ADAPTER PATTERN without custom code.
+        /// </summary>
+        /// <param name="value">
+        /// The object to apply the ADAPTER PATTERN for.
+        /// </param>
         public Adapt(Object value)
         {
             _value = value;
         }
 
+        /// <summary>
+        /// Creates a FLUENT object that holds the mechanism for automatically
+        /// applying the ADAPTER PATTERN without custom code.
+        /// </summary>
+        /// <param name="value">
+        /// The object to apply the ADAPTER PATTERN for.
+        /// </param>
         public static Adapt For(Object value)
         {
             return new Adapt(value);
         }
 
+        /// <summary>
+        /// Applying the ADAPTER PATTERN against <typeparamref name="TInterface"/>.
+        /// </summary>
+        /// <typeparam name="TInterface">
+        /// The interface that will be implemented (ducktyped) by a given object.
+        /// </typeparam>
+        /// <returns>
+        /// The object adapted for <typeparamref name="TInterface"/>.
+        /// </returns>
         public TInterface To<TInterface>()
             where TInterface : class
         {
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AutoAdapter
+
+## Legal
+### License
+Copyright (c) 2021 Copyright Holder All Rights Reserved.
+
+## Quick Start
+### Installation
+### Examples
+See the testing code:
+  - [For C#](./AutoAdapter.DuckTyping.Tests/Convertable.cs)
+  - [For PowerShell](./AutoAdapter.DuckTyping.PowerShell.Tests/AutoAdapter.DuckTyping.PowerShell.Tests.ps1xml)
