190b8f74c1d251967d2b57a220f3c5b44308638b
carrvo <carrvo@gmail.com>
2021-09-19 18:48:57 -0600

    PowerShell supported
    Signed-off-by: carrvo <carrvo@gmail.com>

--- AutoAdapter.PowerShell.Tests/AutoAdapter.Tests.ps1
+++ AutoAdapter.PowerShell.Tests/AutoAdapter.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿Describe "AutoAdapter" {
 	BeforeAll {
-		Import-Module ".\AutoAdapter.PowerShell.Tests.dll"
+		Import-Module ".\AutoAdapter.PowerShell.Tests.psd1"
 	}
 	It "Converts" {
 		$test = New-Test
